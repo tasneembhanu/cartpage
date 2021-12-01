f84a64f5ee1da5a51c170a1a3acf14236703ab0c
tasneembhanu <93967325+tasneembhanu@users.noreply.github.com>
2021-12-01 15:33:48 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Sofa',
+      price: 1400,
+      image: 'https://images.unsplash.com/photo-1616464916265-68fc457bdc70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+    },
+    {
+      id: '2',
+      name: 'Bed',
+      price: 2400,
+      image: 'https://media.istockphoto.com/photos/modern-style-bedroom-picture-id1256466090?b=1&k=20&m=1256466090&s=170667a&w=0&h=tEhMMPuQy8zywLlaYMhQrqqRtOZTzdnuF0Ha418Ip1Y=',
+    },
+    {
+      id: '3',
+      name: 'Living Romm Sofa',
+      price: 1000,
+      image: 'https://media.istockphoto.com/photos/top-view-of-a-luxurious-living-room-in-a-large-house-by-the-sea-picture-id1284941004?b=1&k=20&m=1284941004&s=170667a&w=0&h=p1fReSMFn2cVt75N1FNz5bqeMFlXCokx7Zd7FuFiZeg=',
+    },
+  ],
+};
+export default data;
